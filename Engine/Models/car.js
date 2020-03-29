6604db9f12448cfec134e2ed2086e0a6de4f828f
Aleksander <alekshrastic@gmail.com>
2020-03-29 17:00:46 +0200

    avto za updejtat

--- Engine/Models/car.js
+++ Engine/Models/car.js
@@ -5,15 +5,16 @@ class Car extends CollidableSprite {
 
         this.size = new Vector2D(100, 50);
         this.steer_angle = angle;
+        this.steer_angle_power=0;
+        this.steer_angle_max_power=1.6;
         this.velocity = Vector2D.zeros();
         this.car_direction = Vector2D.zeros();
-
         this.gear = 0;  // -1=reverse, 0=neutral, 1=forward
         this.acceleration = 0.1
-        this.friction = 0.2
+        this.friction = 0.03
+        this.brakeFriction=0.6;
         this.speed = 0;
-        this.maxspeed = 4;
-        this.senzor_reach = 200;
+        this.maxspeed = 5;
     }
 
     draw() {
@@ -28,6 +29,7 @@ class Car extends CollidableSprite {
     }
 
     update(keys){
+        this.adjustSteerPower();
         this.move(keys);
         this.setCarDirection();  
 
@@ -36,13 +38,17 @@ class Car extends CollidableSprite {
     }
 
     move(keys){   
+        if(keys['Spacebar']){
+        this.brake()
+        }
+        else{
         if(keys['ArrowDown'])
             this.moveBack();
         else if(keys['ArrowUp'])
             this.moveOn();
         else 
             this.decelerate();
-
+        }
         if(keys['ArrowLeft'])
             this.moveLeft();
         else if(keys['ArrowRight'])
@@ -51,16 +57,16 @@ class Car extends CollidableSprite {
     moveLeft(){
         if(math.abs(this.speed) > 0){
             switch(this.gear){
-                case 1: this.steer_angle--; break;
-                case -1: this.steer_angle++; break;
+                case 1: this.steer_angle-=this.steer_angle_power; break;
+                case -1: this.steer_angle+=this.steer_angle_power; break;
             }
         }
     }
     moveRight(){
         if(math.abs(this.speed) > 0){
             switch(this.gear){
-                case 1: this.steer_angle++; break;
-                case -1: this.steer_angle--; break;
+                case 1: this.steer_angle+=this.steer_angle_power; break;
+                case -1: this.steer_angle-=this.steer_angle_power; break;
             }
         }
     }
@@ -94,6 +100,24 @@ class Car extends CollidableSprite {
         this.speed = 0;
     }
 
+    brake(){
+        if (this.speed > 0){
+            this.speed -= this.brakeFriction;
+                 }
+         else if(this.speed<0){
+            this.speed += this.brakeFriction;
+         }
+        if(math.abs(this.speed) <= this.friction)
+            this.speed = 0;
+
+
+
+    }
+    adjustSteerPower(){
+        this.steer_angle_power=this.steer_angle_max_power*(1-math.exp(-5*math.abs(this.speed)/this.maxspeed))
+
+
+    }
     checkCollision(colliders){
         colliders.map( (collider) => this.collision(collider));
     }
