edd83627f82bbea533247ac81e08396a2d157599
Matic Isovski <mi6568@student.uni-lj.si>
2020-03-29 11:49:02 +0200

    ne dela wall manager

--- Engine/Manegers/ParkingSpotManeger.js
+++ Engine/Manegers/ParkingSpotManeger.js
@@ -1,28 +1,23 @@
 class ParkingSpotManeger{
     constructor(){
-        this.parkingSpotsArray=[];
-        this.parkingSpotIndex=0;
-        this.createMultiplePargingSpots();
+        this.parkingSpotsArray = [];
+        this.parkingSpotIndex = 0;
     }
 
     createMultiplePargingSpots(){
-       
-        this.createParkingSpot(new Vector2D(20, 20), 2, true);
-        this.createParkingSpot(new Vector2D(20, 110), 1, false);
-        this.createParkingSpot(new Vector2D(20, 290), 1, false);
-        this.createParkingSpot(new Vector2D(20, 380), 1, true);
-        this.createParkingSpot(new Vector2D(20, 470), 1, false);
-        this.createParkingSpot(new Vector2D(20, 560), 1, false);
-        this.createParkingSpot(new Vector2D(20, 650), 2, false);
-
-
-
+        this.createParkingSpot(new Vector2D(200, 20), 1, true);
+        this.createParkingSpot(new Vector2D(200, 110), 1, false);
+        this.createParkingSpot(new Vector2D(200, 290), 1, false);
+        this.createParkingSpot(new Vector2D(200, 380), 1, true);
+        this.createParkingSpot(new Vector2D(210, 380), 3, true);
+        this.createParkingSpot(new Vector2D(210, 470), 3, false);
     }
 
-    createParkingSpot(position,qadrant,occupied){
-        this.parkingSpotsArray[this.parkingSpotIndex]=new ParkingSpot(position,qadrant,occupied);
+    createParkingSpot(position, qadrant, occupied){
+        this.parkingSpotsArray[this.parkingSpotIndex] = new ParkingSpot(position, qadrant, occupied);
         this.parkingSpotIndex++;
     }
+
     draw() {
         for(let spot of this.parkingSpotsArray){
             spot.draw()
