063abf0a68af0edb7406be3912abc9d9b53bc23e
Aleksander <alekshrastic@gmail.com>
2020-04-01 12:02:02 +0200

    spremenjena imena arrayev, posplosen collision detection

--- Engine/Models/parking_spot.js
+++ Engine/Models/parking_spot.js
@@ -1,15 +1,16 @@
 class Parkingspot extends CollidableSprite {
 
     constructor(gameObject,position, qadrant, occupied){
-        super(position, 'rect', new Vector2D(130, 90), ((qadrant - 1) * 90));
+        size=new Vector2D(100, 50)
+        super(position, 'rect', size, (qadrant * 90));
 
-        this.position = position;
         this.occupied = occupied;
         this.objName="parkingspot"
         this.line_width = 10;
 
         this.car_color = color(random(255), random(255), random(255));
         this.gameObject=gameObject
+
     }
 
     update(){
@@ -24,14 +25,12 @@ class Parkingspot extends CollidableSprite {
 
         noStroke(); 
         fill(255, 255, 255);
-        rect(this.size.X / 2, 0, this.size.X, this.line_width);
-        rect(this.line_width / 2, this.size.Y / 2, this.line_width, this.size.Y);
-        rect(this.size.X / 2, this.size.Y, this.size.X, this.line_width);
+        rect(0,0, this.size.X, this.size.Y);
         noFill();
 
         if(this.occupied){
             fill(this.car_color);
-            rect(70, 45, 100, 50);
+            rect(0, 0, this.size.X, this.size.Y);
         }
 
         pop();
